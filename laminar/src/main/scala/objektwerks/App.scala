e65ae763fc16d30e5120fde8f2742f04e9d0a83b
objektwerks <objektwerks@runbox.com>
2025-05-14 09:36:22 -0400

    integrated laminar with plotly

--- laminar/src/main/scala/objektwerks/App.scala
+++ laminar/src/main/scala/objektwerks/App.scala
@@ -1,6 +1,27 @@
 package objektwerks
 
+import com.raquo.laminar.api.L.{*, given}
+
+import org.scalajs.dom
+
 @main def run: Unit =
-  LineChart("line-chart")
-  BarChart("bar-chart")
-  TimeSeriesChart("time-series-chart")
+  val barChartId = "bar-chart"
+  val lineChartId = "line-chart"
+  val timeSeriesChartId = "time-series-chart"
+
+  val container = dom.document.querySelector("#container")
+
+  val plots = div(
+    div( idAttr(barChartId) ),
+    div( idAttr(lineChartId) ),
+    div( idAttr(timeSeriesChartId) )
+  )
+   
+  render(
+    container = container,
+    rootNode = plots
+  )
+
+  BarChart(barChartId)
+  LineChart(lineChartId)
+  TimeSeriesChart(timeSeriesChartId)
